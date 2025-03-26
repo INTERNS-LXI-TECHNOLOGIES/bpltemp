720b50610357c99d5cc3bf5dfb87e987994d6a7d
Arjun7945 <arjunarjups111@gmail.com>
2025-03-26 11:52:52 +0530

    Removed invoicebackend from repository

--- bplinvoice/bpl_invoice_backend/invoicebackend/src/main/java/com/lxisofttech/invoice/web/rest/errors/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Rest layer error handling.
- */
-package com.lxisofttech.invoice.web.rest.errors;
--- bplinvoice/bpl_invoice_backend/invoicebackend/src/main/java/com/lxisofttech/invoice/web/rest/vm/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Rest layer visual models.
- */
-package com.lxisofttech.invoice.web.rest.vm;
