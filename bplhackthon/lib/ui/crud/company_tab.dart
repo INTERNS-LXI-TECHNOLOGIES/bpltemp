701bca92bdeb8c059b1ee9435e3d1d71a2a0c493
Sreeja404 <157472694+Sreeja404@users.noreply.github.com>
2025-04-16 11:22:33 +0530

    updated frontend code

--- bplhackthon/lib/ui/crud/company_tab.dart
+++ bplhackthon/lib/ui/crud/company_tab.dart
@@ -2,24 +2,52 @@
 import 'package:flutter/material.dart';
 
 class CompanyTab extends StatelessWidget {
+  
+  final TextEditingController idController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
         children: [
+         
+          //  ID
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: idController,
+              decoration: InputDecoration(
+                labelText: 'Company ID',
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+        
+          //  Name
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: TextField(
+              controller: nameController,
+              decoration: InputDecoration(
+                labelText: 'Company Name',
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+
           ElevatedButton(
             onPressed: () {
-              // TODO: dispatch CreateCompanyEvent
+              // TODO: dispatch CreateCompanyEvent using:
+             
             },
             child: Text('Create Company'),
           ),
+
           Expanded(
-            child: DataTable(
-              columns: [
-                DataColumn(label: Text('ID')),
-                DataColumn(label: Text('Name')),
-              ],
-              rows: [
+            child: ListView(
+              children: [
                 // TODO: Populate using BLoC stream
               ],
             ),
--- bplhackthon/lib/ui/crud/employee_tab.dart
+++ bplhackthon/lib/ui/crud/employee_tab.dart
@@ -2,6 +2,10 @@
 import 'package:flutter/material.dart';
 
 class EmployeeTab extends StatelessWidget {
+  final TextEditingController idController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController positionController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -9,21 +13,66 @@ class EmployeeTab extends StatelessWidget {
         DropdownButton<String>(
           hint: Text('Select Company'),
           value: null, // TODO: Bind this to selectedCompany
+         
           onChanged: (String? newValue) {
             // TODO: dispatch event to load employees by company
           },
+
           items: [
             // TODO: Populate from CompanyBloc stream
           ],
+
         ),
+
+        //  ID
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            controller: idController,
+            decoration: InputDecoration(
+              labelText: 'Employee ID',
+              border: OutlineInputBorder(),
+            ),
+            keyboardType: TextInputType.number,
+          ),
+        ),
+
+        // Name
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: TextField(
+            controller: nameController,
+            decoration: InputDecoration(
+              labelText: 'Employee Name',
+              border: OutlineInputBorder(),
+            ),
+          ),
+        ),
+
+        //  Position
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+          child: TextField(
+            controller: positionController,
+            decoration: InputDecoration(
+              labelText: 'Position',
+              border: OutlineInputBorder(),
+            ),
+          ),
+        ),
+
+        ElevatedButton(
+          onPressed: () {
+            // TODO: dispatch CreateEmployeeEvent using:
+
+            
+          },
+          child: Text('Add Employee'),
+        ),
+        
         Expanded(
-          child: DataTable(
-            columns: [
-              DataColumn(label: Text('ID')),
-              DataColumn(label: Text('Name')),
-              DataColumn(label: Text('Position')),
-            ],
-            rows: [
+          child: ListView(
+            children: [
               // TODO: Populate using EmployeeBloc stream
             ],
           ),
